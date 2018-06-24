3d103ed6730246e9da03de3d6c1b4d712b72b4b2
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-06-24 10:50:30 -0700

    unsubscribe from mailchimp on delete account

--- src/controllers/user.controller.go
+++ src/controllers/user.controller.go
@@ -3,8 +3,12 @@ package controllers
 import (
   "database/sql"
   "errors"
+  "log"
   "net/http"
+  "os"
 
+  mailchimp "github.com/beeker1121/mailchimp-go"
+  "github.com/beeker1121/mailchimp-go/lists/members"
   "github.com/prosperoa/study-groups/src/models"
   "github.com/prosperoa/study-groups/src/server"
 )
@@ -42,14 +46,40 @@ func GetUsers(page, pageSize int) ([]models.User, int, error) {
 }
 
 func DeleteUser(userID string) (int, error) {
-  result, err := server.DB.Exec("DELETE FROM users WHERE id = $1", userID)
-  rowsAffected, _ := result.RowsAffected()
+  var userEmail string
+
+  err := server.DB.Get(&userEmail, "DELETE FROM users WHERE id = $1 RETURNING email", userID)
 
   switch {
-    case rowsAffected == 0:
+    case err == sql.ErrNoRows:
       return http.StatusBadRequest, errors.New("account doesn't exist")
     case err != nil:
       return http.StatusInternalServerError, errors.New("unable delete account")
+  }
+
+  // remove user from mailchimp list
+  var mailchimpID string
+
+  params := &members.GetParams{Status: members.StatusSubscribed}
+  listMembers, err := members.Get("4d6392ba4d", params)
+
+  if err != nil {
+    for _, v := range listMembers.Members {
+      if v.EmailAddress == userEmail {
+        mailchimpID = v.ID
+        break
+      }
+    }
+
+    if err = mailchimp.SetKey(os.Getenv("MAILCHIMP_API_KEY")); err != nil {
+      log.Println(err.Error())
+    }
+
+    if err = members.Delete("4d6392ba4d", mailchimpID); err != nil {
+      log.Println(err.Error())
+    }
+  } else {
+    log.Println(err.Error())
   }
 
   return http.StatusOK, nil
