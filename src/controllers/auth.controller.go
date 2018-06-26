b2a5e0096275bf2f12a16d870ca54335a0f3b573
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-06-26 12:51:14 -0700

    set created_on, updated_on on sign up

--- src/controllers/auth.controller.go
+++ src/controllers/auth.controller.go
@@ -4,6 +4,7 @@ import (
   "database/sql"
   "errors"
   "log"
+  "time"
   "net/http"
   "os"
 
@@ -57,9 +58,9 @@ func Signup(firstName, lastName, email, password string) (models.User, int, erro
 
   err = server.DB.Get(
     &user,
-    `INSERT INTO users (first_name, last_name, email, password)
-     VALUES ($1, $2, $3, $4) RETURNING *`,
-     firstName, lastName, email, passwordHash,
+    `INSERT INTO users (first_name, last_name, email, password, created_on, updated_on)
+     VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+     firstName, lastName, email, passwordHash, time.Now(), time.Now(),
   )
 
   if err != nil {
