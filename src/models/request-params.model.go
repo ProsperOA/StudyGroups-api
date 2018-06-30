0dfdf8eeee87c1288ff27c2fb74b6c207f02bb67
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-06-30 01:01:00 -0700

    add request params model

---
+++ src/models/request-params.model.go
@@ -0,0 +1,39 @@
+package models
+
+import "strconv"
+
+type UserID struct {
+	Value int `json:"user_id" validate:"required,gt=0"`
+}
+
+type LoginCredentials struct {
+	Email    string `json:"email"    validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=50"`
+}
+
+type SignUpCredentials struct {
+	LoginCredentials
+	FirstName       string `json:"first_name"       validate:"required,min=1,max=20"`
+	LastName        string `json:"last_name"        validate:"max=20"`
+	ConfirmPassword string `json:"confirm_password" validate:"required,min=6,max=50"`
+}
+
+type Account struct {
+	FirstName string `json:"first_name" validate:"required,min=1,max=20"`
+	LastName  string `json:"last_name"  validate:"max=20"`
+	Bio       string `json:"bio"        validate:"max=280"`
+	School    string `json:"school"     validate:"max=20"`
+	Major1    string `json:"major1"     validate:"max=40"`
+	Major2    string `json:"major2"     validate:"max=40"`
+	Minor     string `json:"minor"      validate:"max=40"`
+}
+
+type ChangePassword struct {
+	New     string `json:"new_password"     validate:"required,min=6,max=50,excludesall= "`
+	Confirm string `json:"confirm_password" validate:"required,min=6,max=50,excludesall= "`
+	Current string `json:"current_password" validate:"required"`
+}
+
+func (u UserID) String() string {
+	return strconv.Itoa(u.Value)
+}
